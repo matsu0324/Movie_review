7d10357b602e015b7690a7586625f80ee0ea251e
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-14 08:53:55 +0000

    [Add] user機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  get "homes/about" => "homes#about"
+  resources :users, only: [:index, :show, :edit]
   resources :movies, only: [:index, :show] do
     resources :reviews, only: [:index, :create]
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_010912) do
+ActiveRecord::Schema.define(version: 2020_10_14_063518) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_10_14_010912) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
